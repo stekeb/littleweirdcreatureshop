3871ef9adccb43ad1d9adb093173925fefcdf747
jw <jannik.wienberg@tn.techstarter.de>
2025-09-22 12:07:11 +0200

    sortfield

--- frontend/src/components/StandardView2.jsx
+++ frontend/src/components/StandardView2.jsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom"
 import Header2 from './Header2.jsx'
 import ProductRange from "./ProductRange.jsx";
+import SortField from "./SortField.jsx";
 
 export default function StandardView2({array}) {
     const navigate = useNavigate();
     return (
         <div>
             <Header2 />
-
+            {/* // sortField-Komponente anzeigen */}
+            <SortField />
             <ProductRange array={array}/>
 
         </div>
